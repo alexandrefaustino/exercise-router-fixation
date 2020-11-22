b6b2cfba924faf5c7a6ee85c9a89e289e841fd8d
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-21 21:19:02 -0300

    mudança de componentes js para jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Home from './Home';
-import { BrowserRouter } from 'react-router-dom';
+import About from './About';
+import Users from './Users';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+
         <Home />
       </BrowserRouter>      
     );
