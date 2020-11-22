4f6d5a62d3777ff0ed06db9fddb435119b7f5b08
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-21 21:26:27 -0300

    inclusão da propriedade exact do Route para o diretório /

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Router path="/users" component={Users} />
+        <Route path="/users" component={Users} />
       </BrowserRouter>      
     );
   }
