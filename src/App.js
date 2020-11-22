b10caf2b41df61e49cf14cf0d2a098bc4e176d58
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-21 21:22:41 -0300

    Encapsulamento dos componentes: Home, About e User no Route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Home from './Home';
 import About from './About';
 import Users from './Users';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-
-        <Home />
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Router path="/users" component={Users} />
       </BrowserRouter>      
     );
   }
